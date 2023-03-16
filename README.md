90ff9b1997c4e7a38b5c904eece44db0dbd82555
yanick <me.yan.xia@qq.com>
2023-03-16 10:50:39 +0800

    use libefake time

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 The `TimeTransformer` is a Java agent to manipulate the time returned by `System.currentTimeMillis()` and `System.nanoTime()`. See also [the example project](https://github.com/TOPdesk/time-transformer-examples).
 
+and over [libfaketime](https://github.com/wolfcw/libfaketime)
+
+
 # Usage:
 ## Build Agent
 
@@ -11,13 +14,27 @@ mvn package
 
 ## Run jar with agent
 ```bash
-FAKE_FIXED_TIME=2050-01-01T00:00:00Z java -javaagent:<agent.jar>  -jar <your jar>
+AGENT_LD_PRELOAD=<your libefaketime path> java -javaagent:<agent.jar>  -jar <your jar>
 ```
 
-or 
+
+# demo\
+
+`simple-1.0-SNAPSHOT` just echo current time.
 
 ```bash
-FAKE_OFFSET_TIME='+PT15M' java -javaagent:<agent.jar>  -jar <your jar>
+docker run --rm -it -e AGENT_LD_PRELOAD=/usr/local/lib/faketime/faketime.so \
+-v faketime.so:/usr/local/lib/faketime/faketime.so \
+-v time-transformer-agent-1.4.1-SNAPSHOT.jar:/time-transformer-agent-1.4.1-SNAPSHOT.jar \
+-e JAVA_TOOL_OPTIONS=-javaagent:/time-transformer-agent-1.4.1-SNAPSHOT.jar \
+-e FAKETIME="@2026-01-05 10:30:00" \
+-v simple-1.0-SNAPSHOT.jar:/simple-1.0-SNAPSHOT.jar  \
+adoptopenjdk/openjdk8:alpine-jre \
+java -jar /simple-1.0-SNAPSHOT.jar
 ```
 
-[`Duration format`](http://www.java2s.com/Tutorials/Java/java.time/Duration/2960__Duration.toString_.htm)
+# How it works
+1. Agent init will modify AGENT_LD_PRELOAD -> LD_PRELOAD
+2. Agent call System command `date`, get libfaketime time
+3. Agent compute realNow and fakeTimeNow duration saved
+4. everytime call time will add duration
--- src/main/java/com/topdesk/timetransformer/DefaultTime.java
+++ src/main/java/com/topdesk/timetransformer/DefaultTime.java
@@ -2,11 +2,14 @@ package com.topdesk.timetransformer;
 
 import com.topdesk.timetransformer.agent.DoNotInstrument;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Default implementation of Time which redirects all calls to {@link System#currentTimeMillis} and {@link System#nanoTime}.
@@ -17,15 +20,22 @@ import java.util.concurrent.TimeUnit;
 public enum DefaultTime implements Time {
     INSTANCE;
 
+    private static Duration OFFSET = Duration.ZERO;
+    private static final String LD_ENV_NAME = "AGENT_LD_PRELOAD";
+    private static final String FAKE_TIME_LD_ENV = "LD_PRELOAD";
+
+    static {
+        initOffset();
+    }
+
     @Override
     public long currentTimeMillis() {
-        return fakeZoneDateTime().toInstant().toEpochMilli();
+        return System.currentTimeMillis() + OFFSET.toMillis();
     }
 
     @Override
     public long nanoTime() {
-        final Instant instant = fakeZoneDateTime().toInstant();
-        return (TimeUnit.SECONDS.toNanos(instant.getEpochSecond()) + instant.getNano());
+        return System.nanoTime() + OFFSET.toNanos();
     }
 
     @Override
@@ -33,29 +43,46 @@ public enum DefaultTime implements Time {
         return super.toString() + ", DefaultTime";
     }
 
-    private ZonedDateTime fakeZoneDateTime() {
-        String fakeFixedTime = System.getenv("FAKE_FIXED_TIME");
-        String fakeOffsetTime = System.getenv("FAKE_OFFSET_TIME");
-
-        if (fakeFixedTime != null) {
-            return ZonedDateTime.parse(fakeFixedTime);
+    private static void initOffset() {
+        if (System.getenv(LD_ENV_NAME) == null) {
+            return;
         }
-        ZonedDateTime now = ZonedDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault());
-        if (fakeOffsetTime != null) {
-            char operator = fakeOffsetTime.charAt(0);
-            Duration offset = Duration.parse(fakeOffsetTime.substring(1));
-            switch (operator) {
-                case '+':
-                    now = now.plus(offset);
-                    break;
-                case '-':
-                    now = now.minus(offset);
-                    break;
-            }
+        ZonedDateTime realNow = ZonedDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault());
+        ZonedDateTime offsetNow = fakeZoneDateTime();
+        OFFSET = Duration.between(realNow, offsetNow);
+        System.out.println("offset time: " + OFFSET.toString());
+    }
 
-            return now;
+    private static ZonedDateTime fakeZoneDateTime() {
+        try {
+            Runtime r = Runtime.getRuntime();
+            final Map<String, String> envs = new HashMap<>(System.getenv());
+            envs.put(FAKE_TIME_LD_ENV, envs.get(LD_ENV_NAME));
+            Process p = r.exec("date -u +\"%Y-%m-%dT%H:%M:%SZ\"", toStrings(envs));
+            p.waitFor();
+            BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = b.readLine();
+            b.close();
+            return ZonedDateTime.parse(line.replaceAll("\"", ""));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
         }
+        return null;
+    }
 
-        return now;
+    private static String[] toStrings(final Map<String, String> environment) {
+        if (environment == null) {
+            return null;
+        }
+        final String[] result = new String[environment.size()];
+        int i = 0;
+        for (final Map.Entry<String, String> entry : environment.entrySet()) {
+            final String key = entry.getKey() == null ? "" : entry.getKey();
+            final String value = entry.getValue() == null ? "" : entry.getValue();
+            result[i] = key + "=" + value;
+            i++;
+        }
+        return result;
     }
 }
